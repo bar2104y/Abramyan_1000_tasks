93f26f0d10bab1936dfb83fb1d06ade5e7695d4d
bar2104y <bar2104y@yandex.ru>
2018-10-02 19:47:48 +0300

    Add tasks INTEGER No.24-27 on cpp

---
+++ Results/Cpp/Integer/26.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<< "D (days): ";
+	cin>>a;
+
+	a = ( a + 1 ) % 7;
+	a = fabs(a);
+
+	cout<< a <<endl;
+
+	return 0;
+}
