a127bec9be07b07484a114cd7edfe921c775afa3
bar2104y <bar2104y@yandex.ru>
2018-10-03 21:40:04 +0300

    Update Boolean task No.21 on PHP || Add tasks Boolean No.22-23 on PHP

--- Results/PHP/Boolean/22.php
+++ Results/PHP/Boolean/22.php
@@ -7,7 +7,7 @@ $c = $a % 10;
 $a = $a / 100;
 $a = (int)$a;
 
-if ( $a<$b && $b<$c  ) echo "True";
+if ( ($a<$b && $b<$c) || ($a>$b && $b>$c)  ) echo "True";
 else echo "Fasle";
 
 echo "\n";
---
+++ Results/PHP/Boolean/23.php
@@ -0,0 +1,11 @@
+<?php
+$a = readline("A: ");
+
+$a = strval($a);
+$b = strrev($a);
+
+if ( $a == $b ) echo "True";
+else echo "Fasle";
+
+echo "\n";
+?>
