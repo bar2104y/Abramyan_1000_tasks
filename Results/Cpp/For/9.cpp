811ebf0c98f0e74208ebba15543d66e21248d1b9
bar2104y <bar2104y@yandex.ru>
2018-10-09 07:34:14 +0300

    Add tasks For No.7-9 on C++

---
+++ Results/Cpp/For/9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b,k=0;
+	cout<<"A<B\nA: ";
+	cin>>a;
+    cout<<"B: ";
+	cin>>b;
+
+	for ( int i = a; i <= b; i++ )
+        k += i*i;
+
+    cout<<k<<endl;
+    
+    return 0;
+}
