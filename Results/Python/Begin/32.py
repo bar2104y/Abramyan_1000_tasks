187c129ca989274b7d90f27d829cdc3f2bb5d6fa
bar2104y <bar2104y@yandex.ru>
2018-10-12 22:50:22 +0300

    Update

--- Results/Python/Begin/32.py
+++ Results/Python/Begin/32.py
@@ -1,4 +1,4 @@
 t = int(input("C: "))
 
 t = 1.8*t + 32
-print("F: " + str(t) )
+print("F:",t)
--- Results/Python/Begin/33.py
+++ Results/Python/Begin/33.py
@@ -2,5 +2,5 @@ x = int(input("X (kg): "))
 a = int(input("A (rub): "))
 y = int(input("Y (kg):"))
 a = a/x
-print("1kg - " + str(a) + " rub")
-print("Y kg - " + str(a*y) + " rub")
+print("1kg -",a,"rub")
+print("Y kg -",a*y,"rub")
