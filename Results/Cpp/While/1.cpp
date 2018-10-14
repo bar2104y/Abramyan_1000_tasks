f70e547144323e3a96ff923243a671b3c76b38d7
bar2104y <bar2104y@yandex.ru>
2018-10-14 21:45:52 +0300

    Start tasks While |||| Add task While No.1 on C++ whith binares(Unix)

---
+++ Results/Cpp/While/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    int a,b,k=0;
+    cout<<"A<B\nA, B:\n";
+    cin>>a>>b;
+
+    while (a >= b)
+    {
+        a -= b;
+        k++;
+    }
+
+    cout<<"in A - "<<k<<" B"<<endl;
+}
