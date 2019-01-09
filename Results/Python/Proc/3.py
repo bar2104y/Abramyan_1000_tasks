a4c9dadcf451b6d089ca4fcb5c5f06f051190c37
Баричев Юрий <bar2104y@yandex.ru>
2019-01-09 17:56:11 +0300

    Add tasks Proc No.1&2

---
+++ Results/Python/Proc/3.py
@@ -0,0 +1,6 @@
+import math
+def Mean(x,y):
+    global AMean
+    global GMean
+    AMean = (x+y)/2
+    GMean = math.sqrt(x*y)
---
+++ Results/Python/Proc/4.py
@@ -0,0 +1,6 @@
+import math
+def TrianglePS(a):
+    global p
+    global s
+    p = a*3
+    s = (a**2)*math.sqrt(3)/4
