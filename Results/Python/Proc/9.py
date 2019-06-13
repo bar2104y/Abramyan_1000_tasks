1725c3ca21d1bf65169af0ae3ef36eabaeb66be3
Баричев Юрий <bar2104y@yandex.ru>
2019-06-13 23:56:14 +0300

    Add Proc 9 on Python

---
+++ Results/Python/Proc/9.py
@@ -0,0 +1,14 @@
+def AddLeftDigit(d,k):
+    cnt = 0
+    k1 = k
+    while k1 > 0:
+        cnt += 1
+        k1 //= 10
+    k += d*(10**cnt)
+    return k
+
+k = 456       #INPUT
+da = [3,2,1]  #D1,D2...
+for i in da:
+    k = AddLeftDigit(i,k)
+    print(k)
