04b2a1569f31c3785835a5aae6cde3e854c41864
Баричев Юрий <bar2104y@yandex.ru>
2019-06-17 22:29:26 +0300

    Add Proc 54 on Python

---
+++ Results/Python/Proc/54.py
@@ -0,0 +1,23 @@
+def IsLeapYear(y):
+    if y % 4 != 0: return(False)
+    if y%100 == 0 and y%400 != 0: return(False)
+    return(True)
+
+def MonthDays(m,y):
+    if m == 2:
+        return(29 if IsLeapYear(y) else 28 )
+    else:
+        return (31 if (m%7)%2 != 0 or m == 7 else 30)
+    
+def PrevDate(d,m,y):
+    if d == 1:
+        if m == 1:
+            y -= 1
+            m = 12
+        else:
+            m -= 1
+        d = MonthDays(m,y)
+    else: d -= 1
+    return(d,m,y)
+
+print(PrevDate(1,1,2011))
