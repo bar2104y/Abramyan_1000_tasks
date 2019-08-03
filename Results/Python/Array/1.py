b6acdd94c07c1f466c26672a7154f9a7a4fc7ab2
Баричев Юрий <bar2104y@yandex.ru>
2019-08-03 23:26:11 +0300

    Add Array  No.1-2 on Python

---
+++ Results/Python/Array/1.py
@@ -0,0 +1,7 @@
+a = []
+n = int(input("N: "))
+
+for i in range(n):
+    a.append(i*2 + 1)
+
+print(a)
---
+++ Results/Python/Array/2.py
@@ -0,0 +1,7 @@
+a = []
+n = int(input())
+
+for i in range(1,n+1):
+    a.append(2**i)
+
+print(a)
