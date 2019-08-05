f7baa41347de6952f9571a5e4dd64d3e034f4bff
Баричев Юрий <bar2104y@yandex.ru>
2019-08-05 16:52:10 +0300

    Add Array No.22-23 on Python3

---
+++ Results/Python/Array/23.py
@@ -0,0 +1,14 @@
+import random
+n = int(input("N: "))
+a = []
+for i in range(n):
+    a.append(int(random.randint(0, 100)))
+print(a)
+
+s = 0
+k = int(input("K: "))
+l = int(input("L: "))
+
+for i in range(n): s+=a[i]
+
+print((s-a[k]-a[l])/(n-2))
