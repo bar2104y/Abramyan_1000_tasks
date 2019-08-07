6965c68749e9a241b0ceb00f8c4851768ed38afb
Баричев Юрий <bar2104y@yandex.ru>
2019-08-07 18:10:56 +0300

    Add

---
+++ Results/Python/Array/29.py
@@ -0,0 +1,18 @@
+import random
+n = int(input("N: "))
+a = []
+
+m = -1
+i = 1
+
+################
+for i in range(n):
+    a.append(random.randint(0,100))
+print(a)
+################
+
+while i < n:
+    if a[i] > m: m = a[i]
+    i += 2 
+
+print(m)
