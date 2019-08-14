ecc88c18a5e24302ff99e0c72197b57854358656
bar2104y <bar2104y@yandex.ru>
2019-08-14 18:18:46 +0300

    Add

---
+++ Results/Python/Array/61.py
@@ -0,0 +1,17 @@
+from genarr import genRandomArr
+n = int(input("N: "))
+
+a = genRandomArr(n,0,5)
+a[0] = 0
+print(a)
+
+b = []
+sum = 0
+for i in range(n):
+    sum += a[i]
+
+for i in range(1,n):
+    b.append(sum/(n-i))
+    sum -= a[i]    
+
+print(b)
