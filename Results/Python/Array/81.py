9559224f160c2e9eb2b33a2b1f32655a5abbd837
bar2104y <bar2104y@yandex.ru>
2019-08-15 10:23:14 +0300

    Add

---
+++ Results/Python/Array/81.py
@@ -0,0 +1,17 @@
+#Последовательный сдвиг, используя решение из №79
+from genarr import genLinearArr
+n = int(input("N: "))
+k = int(input("K: "))
+
+a = genLinearArr(n)
+print(a)
+
+for j in range(k):
+    tmp = a[0]
+
+    for i in range(n):
+        tmp1 = a[(i+1)%n]
+        a[(i+1)%n] = tmp
+        tmp = tmp1  
+        
+print(a)
