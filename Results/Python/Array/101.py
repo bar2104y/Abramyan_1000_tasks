524bab5f468251a0a5b4b06a55139f3b7b45d284
bar2104y <bar2104y@yandex.ru>
2019-08-16 16:31:25 +0300

    Теперь все честно

--- Results/Python/Array/101.py
+++ Results/Python/Array/101.py
@@ -5,8 +5,15 @@ k = int(input("K: "))
 a = genLinearArr(n)
 print(a)
 
-a.insert(k, 0)
+#a.insert(k, 0)
+tmp1 = a[k]
+a[k] = 0
+for i in range(k,n-1):
+    tmp = a[i+1]
+    a[i+1] = tmp1
+    tmp1 = tmp
 
+a.append(tmp1)
 
 
 print(a)
