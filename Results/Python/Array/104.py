9d46ade6bdbdfd7f91db38a073f05b83e58577a8
bar2104y <bar2104y@yandex.ru>
2019-08-16 17:37:39 +0300

    Добавлено

---
+++ Results/Python/Array/104.py
@@ -0,0 +1,12 @@
+from genarr import genLinearArr
+n = int(input("N: "))
+k = int(input("K: "))
+m = int(input("M: "))
+
+a = genLinearArr(n)
+print(a)
+
+for i in range(m):
+    a.insert(k,0)
+
+print(a)
