9ddec3e7d67a77f567329636fc4e97850a23e87b
bar2104y <bar2104y@yandex.ru>
2019-08-18 07:00:08 +0300

    Серии целых чисел

---
+++ Results/Python/Array/128.py
@@ -0,0 +1,29 @@
+from genarr import genRandomArr
+n = int(input("N: "))
+
+a = genRandomArr(n,0,3)
+a = [1, 1, 2, 0, 2, 2, 2, 3, 3, 3,3]
+print(a)
+
+i,cnt,l, li = 0,1,0, 0
+while i < len(a)-1:
+    if a[i] == a[i+1]:
+        cnt += 1
+    else:
+        if cnt > l:
+            mi,l = li, cnt
+            d = a[i]
+        cnt = 1
+        li = i+1
+    i += 1
+
+if a[len(a)-1] == a[len(a)-2]:
+    cnt += 1
+    
+if cnt > l:
+    mi,l = li, cnt
+    d = a[i]
+
+a.insert(li, d)
+
+print(a)
