225e68e4c081957e4ecf5207c4bab87b23cd529d
Yury Barichev <bar2104y@yandex.ru>
2019-08-29 03:54:46 +0300

    Анализ матриц

---
+++ Results/Cpp/Matrix/46.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream> 
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	int m,n;
+	cout<<"(строки)  M: "; cin>>m;
+	cout<<"(столбцы) N: "; cin>>n;
+	
+	//int h[m][n];
+	int h[2][2] = {{9,0}, {8,2}};
+	//h[строка][столбец]
+	//М строк на N столбцов
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			//h[i][j] = rand() % 10;
+			cout<<h[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+	for ( int i = 0; i<m; i++)
+	{
+		int ms = 0;
+		int jj = 0;
+		for ( int j = 0; j < n; j++ )
+			if ( h[i][j] > ms )
+			{
+				ms = h[i][j];
+				jj = j;
+			}
+
+		bool f = true;
+		for (int k = 0; k<m; k++ )
+			if (h[k][jj] < ms)
+				f = false;
+		
+		if (f) cout<<ms<<endl;					
+	}	
+}
